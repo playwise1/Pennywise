1baea50e3fce2fa6313b03dd6f539cb7861206e0
playwise <amazingali382@gmail.com>
2025-12-17 15:06:47 +0530

    updated splash screen

--- app/src/main/java/com/example/pennywise/ui/screens/SplashScreen.kt
+++ app/src/main/java/com/example/pennywise/ui/screens/SplashScreen.kt
@@ -4,14 +4,18 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.pennywise.R
 import kotlinx.coroutines.delay
 
@@ -28,10 +32,21 @@ fun SplashScreen(onTimeout: () -> Unit) {
             contentDescription = "App Logo",
             modifier = Modifier.size(400.dp)
         )
+
+        Text(
+            text = "Developed by Team JAS™",
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 48.dp),
+            color = Color.Gray,
+            fontSize = 12.sp,
+            fontWeight = FontWeight.SemiBold,
+            letterSpacing = 1.sp
+        )
     }
 
     LaunchedEffect(Unit) {
-        delay(700)
+        delay(1000)
         onTimeout()
     }
 }
